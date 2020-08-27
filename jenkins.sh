332de56dfc93b7ce27fb24d8305f846439c1c01e
Omar Bazaid <omar.vip3@gmail.com>
2020-08-27 16:26:11 +0300

    fix time 60

--- jenkins.sh
+++ jenkins.sh
@@ -5,7 +5,7 @@ source <(kubectl completion bash)
 kubectl apply -f jenkins.namespace.yaml
 kubectl apply -f jenkins.helm.yaml
 kubectl -n jenkins apply -f clusterRole.yaml
-WAIT=150
+WAIT=60
 echo "Sleeping for $WAIT"
 sleep $WAIT
 echo "Making progress"

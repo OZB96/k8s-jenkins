3fbbc787d192bf4567db9a8a01d5a642a736b169
Ubuntu <ubuntu@ip-10-0-20-25.me-south-1.compute.internal>
2020-08-30 19:08:57 +0000

    add cluster

--- query.sh
+++ query.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-POD=$(kubectl get pods -n jenkins -o json | jq '.items[1].metadata.name' | xargs)
+POD=$(sudo kubectl get pods -n jenkins -o json | jq '.items[1].metadata.name' | xargs)
 echo "Jenkins credentials"
-while [[ "$(kubectl -n jenkins --container jenkins exec $POD -- env | grep ADMIN_USER | sed 's/.*=//')" == "" ]] ; do
+while [[ "$(sudo kubectl -n jenkins --container jenkins exec $POD -- env | grep ADMIN_USER | sed 's/.*=//')" == "" ]] ; do
 sleep 2;
-POD=$(kubectl get pods -n jenkins -o json | jq '.items[1].metadata.name' | xargs)
+POD=$(sudo kubectl get pods -n jenkins -o json | jq '.items[1].metadata.name' | xargs)
 done
 
-echo username: $(kubectl -n jenkins --container jenkins exec $POD -- env | grep ADMIN_USER | sed 's/.*=//')
-echo password: $(kubectl -n jenkins --container jenkins exec $POD -- env | grep ADMIN_PASSWORD | sed 's/.*=//')
-echo port: $(kubectl get service jenkins -n jenkins -o json | jq '.spec.ports[0].nodePort')
+echo username: $(sudo kubectl -n jenkins --container jenkins exec $POD -- env | grep ADMIN_USER | sed 's/.*=//')
+echo password: $(sudo kubectl -n jenkins --container jenkins exec $POD -- env | grep ADMIN_PASSWORD | sed 's/.*=//')
+echo port: $(sudo kubectl get service jenkins -n jenkins -o json | jq '.spec.ports[0].nodePort')
 

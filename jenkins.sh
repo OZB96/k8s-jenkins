4acdcbb3487bba6f7e5bbe1147f451932692753c
Omar Bazaid <omar.vip3@gmail.com>
2020-08-31 17:00:19 +0300

    remove sudo

--- jenkins.sh
+++ jenkins.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 k3d cluster create jenkins -v /var/run/docker.sock:/var/run/docker.sock --agents 3 -p 30000-32767:30000-32767@server[0] -p 80:80@loadbalancer
-source <(sudo k3s completion bash)
-source <(sudo kubectl completion bash)
-sudo kubectl apply -f jenkins.namespace.yaml
-sudo kubectl apply -f jenkins.helm.yaml
-sudo kubectl -n jenkins apply -f clusterRole.yaml
-sudo kubectl -n jenkins apply -f ingress.yaml
+source <(k3d completion bash)
+source <(kubectl completion bash)
+kubectl apply -f jenkins.namespace.yaml
+kubectl apply -f jenkins.helm.yaml
+kubectl -n jenkins apply -f clusterRole.yaml
+kubectl -n jenkins apply -f ingress.yaml
 sleep 60
 echo "Making progress"
 . query.sh

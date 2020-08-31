2a127aa9206c04e5088284823f32493fa12a32a1
Omar Bazaid <omar.vip3@gmail.com>
2020-08-31 18:55:22 +0300

    fix

--- jenkins.sh
+++ jenkins.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-k3d cluster create jenkins -v /var/run/docker.sock:/var/run/docker.sock --agents 3 -p 30000-32767:30000-32767@server[0] -p 80:80@loadbalancer
+k3d cluster create jenkins -v /etc/machine-id:/etc/machine-id:ro -v /var/log/journal:/var/log/journal:ro -v /var/run/docker.sock:/var/run/docker.sock --agents 3 -p 30000-32767:30000-32767@server[0] 
 source <(k3d completion bash)
 source <(kubectl completion bash)
 kubectl apply -f jenkins.namespace.yaml
@@ -8,5 +8,5 @@ kubectl -n jenkins apply -f clusterRole.yaml
 sleep 60
 echo "Making progress"
 . query.sh
-kubectl -n jenkins apply -f service.yaml
+#kubectl -n jenkins apply -f service.yaml
 kubectl -n jenkins apply -f ingress.yaml
